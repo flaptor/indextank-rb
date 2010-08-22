da1e6188b52a3054d7c9cda70e4afc391b42e8d8
Terence Lee <hone02@gmail.com>
2010-08-22 01:13:46 -0500

    add Function#add

--- lib/indextank/function.rb
+++ lib/indextank/function.rb
@@ -11,6 +11,16 @@ module IndexTank
       @conn    = IndexTank.setup_connection(@uri)
     end
 
+    def add(options = {})
+      options.merge!(:definition => self.definition)
+      resp = @conn.put do |req|
+        req.url ''
+        req.body = options.to_json
+      end
+
+      resp.status == 200
+    end
+
     def ==(other)
       self.uri == other.uri and
         self.index == other.index
---
+++ spec/lib/indextank/function_spec.rb
@@ -0,0 +1,54 @@
+require File.expand_path('../../../spec_helper', __FILE__)
+
+describe IndexTank::Function do
+  before do
+    @stubs = Faraday::Adapter::Test::Stubs.new
+    stub_setup_connection
+    @function = IndexTank::Client.new("http://:xxxx@dstqe.api.indextank.com").indexes('new-index').functions(0, '-age')
+    @path_prefix = '/v1/indexes/new-index/functions/0/'
+  end
+
+  describe "function management" do
+    describe "#add" do
+      context "function saved" do
+        before do
+          @stubs.put(@path_prefix) { [200, {}, ''] }
+        end
+
+        it "should return true" do
+          @function.add.should be_true
+        end
+      end
+
+      context "index is initializing" do
+        before do
+          @stubs.put(@path_prefix) { [409, {}, ''] }
+        end
+
+        it "should return false" do
+          @function.add.should be_false
+        end
+      end
+
+      context "invalid or missing argument" do
+        before do
+          @stubs.put(@path_prefix) { [400, {}, ''] }
+        end
+
+        it "should return false" do
+          @function.add.should be_false
+        end
+      end
+
+      context "no index existed for the given name" do
+        before do
+          @stubs.put(@path_prefix) { [404, {}, ''] }
+        end
+
+        it "should return false" do
+          @function.add.should be_false
+        end
+      end
+    end
+  end
+end
